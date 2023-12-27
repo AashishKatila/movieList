a79b67c14dcad9201cc70afde2d2d24cf0b29f8c
Aashish Katila <aashishkatila@gmail.com>
2023-12-27 17:07:49 +0545

    used localStorage for holdlist

--- src/context/GlobalState.jsx
+++ src/context/GlobalState.jsx
@@ -1,26 +1,37 @@
-import React,{createContext, useContext,useReducer} from 'react'
-import AppReducer from './AppReducer';
+import React, { createContext, useEffect, useReducer } from "react";
+import AppReducer from "./AppReducer";
 
 //Initial State
 const initialState = {
-    holdList: [],
-    watchedMovie: [],
-}
+  holdList: localStorage.getItem("holdList") ? JSON.parse(localStorage.getItem('holdList')) : [],
+  watchedMovie: localStorage.getItem("watchedMovie") ? JSON.parse(localStorage.getItem('watchedMovie')) : [],
+};
 
 export const GlobalContext = createContext(initialState);
 
+//Manage Global State
+export const GlobalProvider = (props) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
 
+  useEffect(() => {
+    localStorage.setItem("holdList", JSON.stringify(state.holdList));
+    localStorage.setItem("watchedMovie", JSON.stringify(state.watchedMovie));
+}, [state]);
 
-export const GlobalProvider = (props) =>{
-    const [state,dispatch] = useReducer(AppReducer,initialState)
+  //To add movie in holdList
+  const addMovieToHoldList = (movie) => {
+    dispatch({ type: "ADD_MOVIE_TO_HOLDLIST", payload: movie });
+  };
 
-    const addMovieToHoldList = (movie) =>{
-        dispatch({type: "ADD_MOVIE_TO_HOLDLIST", payload:movie})
-    }
-
-    return(
-        <GlobalContext.Provider value={{holdList: state.holdList, watchedMovie: state.watchedMovie, addMovieToHoldList:addMovieToHoldList}}>
-            {props.children}
-        </GlobalContext.Provider>
-    )
-}
+  return (
+    <GlobalContext.Provider
+      value={{
+        holdList: state.holdList,
+        watchedMovie: state.watchedMovie,
+        addMovieToHoldList: addMovieToHoldList,
+      }}
+    >
+      {props.children}
+    </GlobalContext.Provider>
+  );
+};
