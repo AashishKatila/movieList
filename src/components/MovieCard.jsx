b86bab31410138f4dad7c7939f14f96601497c8e
Aashish Katila <aashishkatila@gmail.com>
2024-01-10 19:11:48 +0545

    Added bg in holdlist/watched movies not having poster image

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,10 +4,15 @@ import MovieControl from "./MovieControl";
 const MovieCard = ({ movie, type }) => {
   return (
     <div className="relative group">
-      <img
-        src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
-        alt={`${movie.title} `}
-      />
+      {movie.poster_path ? (
+        <img
+          src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+          alt={`${movie.title} `}
+        />
+      ) : (
+        <div className="w-52 md:h-80 h-72 object-fill bg-gray-400 block ">
+          <div className="absolute top-36 left-20 text-white font-3xl font-bold">{movie.title}</div></div>
+      )}
       <MovieControl movie={movie} type={type} />
     </div>
   );
